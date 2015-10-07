e1b9c568e12c8a1c8da1730168f1d316bcc070a2
deniseiras <denis.eiras@gmail.com>
2015-10-07 11:28:36 -0300

    arrumando a casa

--- bufr/stationCords/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gfortran -cpp -ffixed-line-length-none -g -fbacktrace -fcheck=bounds iso_varying_string.f95 benchmarking.f90 date_utils.f90 inmet_stations.f90 get_station_vars.f90 -o generateVars.x
-rm *.mod
